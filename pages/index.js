e5a8f1cfdbd0336c6d6c497230d06d53655dd4af
erfanmah7 <efn8mah@yahoo.com>
2024-11-18 22:37:15 +0330

    get todos api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <>hello</>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function fetching() {
+      const res = await fetch("/api/todos");
+      const data = await res.json();
+      setTodos(data);
+    }
+    fetching();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>Todos</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
